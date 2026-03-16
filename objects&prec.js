07f68831812ed39797022265f4a404c09c793451
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-03-16 11:48:11 +0530

    Add person object with properties and email key

---
+++ objects&prec.js
@@ -0,0 +1,17 @@
+const person = {
+  name : "shubham",
+  age : 25,
+  address :"Indore",
+  phone : 123456789
+}
+// adding a new key & value
+
+person['email'] = "sanukayda@gmail.com"
+
+
+// exces key and there value
+console.log(person.name)
+console.log(person ['age'])
+console.log(person.address)
+// exces the object
+console.log(person)
