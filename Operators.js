6ec929ad572f50f274e2f33ca142bf0e521127da
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-03-16 12:40:04 +0530

    Operators

---
+++ Operators.js
@@ -0,0 +1,30 @@
+// arthmetic operator
+let c=25;
+let b=3;
+
+console.log("Arthmetic operator")
+console.log("c+b:", c+b)
+console.log("c-b:", c-b)
+console.log("c*b:", c*b)
+console.log("c**b:", c**b)
+console.log("c/b:", c/b)
+console.log("c%b:", c%b)
+console.log("++c:",++c)
+console.log("c++:", c++)
+console.log("now c is:",c)
+console.log("c--:", c--)
+console.log("c:", c)
+console.log("--c:", --c)
+console.log("now c is:",c)
+
+console.log("Assiment Operator")
+let a =5;
+a+=20;
+// console.log( "+=" ,a+=5)
+console.log("a+=:",a)
+a-=20
+console.log("a-=:",a)
+a*=20
+console.log("a*=:" ,a)
+a/=20
+console.log("a/=:",a)
