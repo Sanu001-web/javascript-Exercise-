b452bd2445815300133749d8c4cc94f5571a2d1a
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-03-19 12:12:24 +0530

    Update Loops

--- Loops.js
+++ Loops.js
@@ -6,3 +6,9 @@ for(let i=0; i<=n; i++){
     sum +=i
 }
 console.log("The Sum of first " + n + " natural numer is:" +sum)
+// print number that user given
+let n=prompt("Enter the value of n:")
+n=Number.parseInt(n);
+for(let i=0; i<n; i++){
+    console.log("Print the given number:" +i)
+}
