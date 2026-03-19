c965bba2f1133b3f938d51c98105755c4906ef03
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-03-19 12:00:36 +0530

    Create Loops and for loop Program Add natural number

---
+++ Loops.js
@@ -0,0 +1,8 @@
+// program to add first natural number
+let sum=0;
+let n=prompt("Enter the value of n:")
+n=Number.parseInt(n);
+for(let i=0; i<=n; i++){
+    sum +=i
+}
+console.log("The Sum of first " + n + " natural numer is:" +sum)
