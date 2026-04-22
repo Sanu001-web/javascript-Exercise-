655cee1a119f11e237adb872057ec7faeef59235
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-04-22 14:41:38 +0530

    Implement leap year checker
    Added a program to check if a year is a leap year.

--- JavaScript Prectice/Operators & Conditional Statements.js
+++ JavaScript Prectice/Operators & Conditional Statements.js
@@ -96,3 +96,32 @@ else
   console.log("The ticket price for" ,age,"is: 15")
 
 }
+
+// Write a program that determines if a year is a leap year.
+
+// let isLeap = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+
+let year =prompt("Enter the age:")
+year=Number.parseInt(year)
+
+if(year<0)
+{
+  console.log("Please enter the valid year!")
+}
+else if(year%400==0)
+{
+  console.log(year," is a leap year!")
+}
+else if(year%100==0)
+{
+console.log(year," is not a leap year!")
+}
+else if(year%4==0){
+  console.log(year," is a leap year!")
+}
+else {
+  console.log(year, "is NOT a leap year!");
+}
+
+
+
