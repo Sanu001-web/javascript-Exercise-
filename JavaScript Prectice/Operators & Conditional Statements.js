e139f0215c5690c246727e838a6e4b56408de15c
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-04-22 12:46:39 +0530

    Implement number checks and comparisons
    Added programs to check if a number is positive or negative, even or odd, and to determine the greater of two numbers.

--- JavaScript Prectice/Operators & Conditional Statements.js
+++ JavaScript Prectice/Operators & Conditional Statements.js
@@ -1,5 +1,35 @@
-// let number = 5
+// Write a program that determines whether a given number is positive or negative.
 let num=prompt("Enter the number:")
 num=Number.parseInt(num)
 
 num >0?console.log(num,"is positive"):console.log(num,"is negative")
+
+
+// Write a program that checks if a number is even or odd.
+
+let num=prompt("Enter the Number:")
+num=Number.parseInt(num);
+if(num%2==0)
+{
+  console.log(num,"is even number.")
+}
+else{
+  console.log(num,"Is odd number")
+}
+
+// Write a program to determine the greater of two numbers.
+
+let a=prompt("Enter the first number:")
+a=Number.parseInt(a)
+let b=prompt("Enter the second number:")
+b=Number.parseInt(b)
+
+if(a>b){
+  console.log(a +" is grater than",+b)
+}
+else if(a>=b){
+  console.log(a +" Equals to",+b)
+}
+else{
+  console.log(a +" is not grater & than equals to",+b)
+}
