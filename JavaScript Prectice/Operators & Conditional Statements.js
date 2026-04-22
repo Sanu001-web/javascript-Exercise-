cb9dc05d78fec5dbac7f4ddce1ae5801d4b84356
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-04-22 14:01:22 +0530

    Implement ticket price calculation based on age
    Add age-based ticket pricing logic with validation.

--- JavaScript Prectice/Operators & Conditional Statements.js
+++ JavaScript Prectice/Operators & Conditional Statements.js
@@ -67,3 +67,32 @@ if (grade >= 0 && grade <= 10) {
 else {
   console.log("Please enter a valid grade (0–10)");
 }
+
+// Write a program that calculates the ticket price based on age with the following conditions: age below 12 pay a ticket price of 5, age below 18 pay a ticket price of 10, age below 60 pay a ticket price of 20, age over 60 play a ticket price of 15.
+
+let age =prompt("Enter your age:")
+age= Number.parseInt(age)
+
+
+if(age < 0)
+{
+ console.log("Please enter the valid age:")
+}
+else if(age<=12)
+{
+  console.log("The ticket price for" ,age,"is: 5")
+}
+else if(age<18)
+{
+  console.log("The ticket price for" ,age,"is: 10")
+}
+else if(age<60)
+{
+  console.log("The ticket price for" ,age,"is: 20")
+
+}
+else 
+{
+  console.log("The ticket price for" ,age,"is: 15")
+
+}
