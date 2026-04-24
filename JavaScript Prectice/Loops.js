d64c83e6114ddfe6437679ef1925679a461717f6
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-04-24 14:29:49 +0530

    Implement FizzBuzz in Loops.js
    Add FizzBuzz logic to iterate from 1 to 45.

--- JavaScript Prectice/Loops.js
+++ JavaScript Prectice/Loops.js
@@ -8,3 +8,24 @@ if(i%2==0) //if(i%2!=2) this for 'odd nummber'
   console.log("Numbers:",i)
 }
 }
+
+ // Iterate through all numbers from 1 to 45. Print the following:
+// For multiples of 3 print “Fizz”
+// For multiples of 5 print “Buzz”
+// For multiples of 3 and 5 print “FizzBuzz”
+
+for(let i=1;i<=45;i++)
+{
+  if(i%3==0 && i%5==0)
+  {
+    console.log("fizzBuzz")
+  }
+  else if(i%3==0)
+  {
+    console.log("fizz")
+  }
+  else if(i%5==0)
+  {
+    console.log("Buzz")
+  }
+}
