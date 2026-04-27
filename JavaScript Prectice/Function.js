003ddca5c4796dd39c9552d9ba7ef1af60888c61
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-04-27 14:52:54 +0530

    Add function to calculate average plus one

---
+++ JavaScript Prectice/Function.js
@@ -0,0 +1,11 @@
+function run(a,b){
+  let sum =a+b;
+  let average=sum/2;
+  let averagePlusOne=average+1;
+  console.log("AveragePlusOne:",averagePlusOne)
+  return averagePlusOne
+}
+run(4,8);
+run(78,8);
+run(7,8);
+run(7,7);
