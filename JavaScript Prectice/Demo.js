a8659b3761c65647dcc32ac49b97e9428c5eb807
sanu kayda <sanukayda@gmail.com>
2026-05-18 15:11:18 +0530

    add some prac files

---
+++ JavaScript Prectice/Demo.js
@@ -0,0 +1,14 @@
+console.log("hello Javascript !");
+function greet(name){
+    console.log("welcome to javascript",name);
+    return 20;
+}
+let rslt = greet("sachin");
+console.log(rslt);
+
+ let  abc =() =>{
+    console.log("helloooooooo");     
+}
+abc();
+           
+
