f5a5787c5ac1a66150121d2c0126be98ff56566f
sanu kayda <sanukayda@gmail.com>
2026-05-19 15:48:49 +0530

    adding exercises

--- JavaScript Prectice/lesson-2-prac.js
+++ JavaScript Prectice/lesson-2-prac.js
@@ -7,11 +7,27 @@ console.log("adding new iteams:");
 console.log("result 2i:",Math.round(((2095+799+499)*0.1)+Math.round(2095+799+499))/100); 
 console.log("  ");     
 console.log("Diff excercise:");
-let celsius =prompt("Enter the temperature in celsius");
-console.log("Temperature in Fahrenheit:",celsius*(9/5)+32);
-let fahrenheit =prompt("Enter the temperature in Fahrenheit");
-console.log("Temperature in Celsius:",(fahrenheit-32)*5/9);
-console.log("  ");     
+
+// let celsius =prompt("Enter the temperature in celsius");
+// console.log("Temperature in Fahrenheit:",celsius*(9/5)+32);
+// let fahrenheit =prompt("Enter the temperature in Fahrenheit");
+// console.log("Temperature in Celsius:",(fahrenheit-32)*5/9);
+
+console.log("strings");
+console.log('shaan '+'kayda')
+console.log('some'+'one')
+console.log(typeof 45)
+console.log(22+'some ')
+console.log("result 1 :" +'$' + (2095+799)/100)
+console.log("result 2:" +'$' + ((2095+799)*0.1)/100)
+console.log("result 3:" +'$' + (((2095+799)*0.1)+(2095+799))/100);
+
+console.log('Items('+(1+1)+'):' + '$'+(2095+799)/100)
+// alert('Items('+(1+1)+'):' + '$'+(2095+799)/100);
+console.log(`shaan `+ `is ok`) //Template String
+ 
+console.log('Interpolation:::')
+console.log(`Items (${1+1}): $${(2095+799)/100}`);
 console.log("Break");     
 
 
