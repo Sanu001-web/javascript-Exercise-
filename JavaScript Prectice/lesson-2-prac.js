1ff054b56654c6095035386a063ee7ef3382e8e7
sanu kayda <sanukayda@gmail.com>
2026-05-19 14:44:07 +0530

    rename file name and add some practice example

---
+++ JavaScript Prectice/lesson-2-prac.js
@@ -0,0 +1,17 @@
+console.log("result 2f:",(1899+2095+799)/100); 
+console.log("result 2g:",(1899+2095+799+499)/100); 
+console.log("result 2h:",Math.round((1899+2095+799+499)*0.1)/100); 
+console.log("result 2i:",Math.round(((1899+2095+799+499)*0.1)+Math.round(1899+2095+799+499))/100); 
+console.log("  ");     
+console.log("adding new iteams:");     
+console.log("result 2i:",Math.round(((2095+799+499)*0.1)+Math.round(2095+799+499))/100); 
+console.log("  ");     
+console.log("Diff excercise:");
+let celsius =prompt("Enter the temperature in celsius");
+console.log("Temperature in Fahrenheit:",celsius*(9/5)+32);
+let fahrenheit =prompt("Enter the temperature in Fahrenheit");
+console.log("Temperature in Celsius:",(fahrenheit-32)*5/9);
+console.log("  ");     
+console.log("Break");     
+
+
