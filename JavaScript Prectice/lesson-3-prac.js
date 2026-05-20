6a351d471693def01e09b6f1e6745102a154f5b8
sanu kayda <sanukayda@gmail.com>
2026-05-20 14:28:28 +0530

    modify exersice

--- JavaScript Prectice/lesson-3-prac.js
+++ JavaScript Prectice/lesson-3-prac.js
@@ -1,18 +1,20 @@
 console.log("JavaScript Exersice 03 !")
 
-console.log('3a:\n'+'My name is')
-console.log('3b:\n'+'shaan')
-console.log('3c:\n' +'My name is' +' shaan')
-console.log('3d:\n' +'Total cost: '+ '$'+(5+3))
-console.log(`3e:\nTotal cost: $${5+3}`)
+console.log('3a:\n' + 'My name is')
+console.log('3b:\n' + 'shaan')
+console.log('3c:\n' + 'My name is' + ' shaan')
+console.log('3d:\n' + 'Total cost: ' + '$' + (5 + 3))
+console.log(`3e:\nTotal cost: $${5 + 3}`)
 // alert(`3f:\nTotal cost: $${5+3}`)
-console.log('3g:\n' +'Total cost: '+ '$'+(599+295)/100)
-console.log(`3h:\nTotal cost: $${(599+295)/100}`);
+console.log('3g:\n' + 'Total cost: ' + '$' + (599 + 295) / 100)
+console.log(`3h:\nTotal cost: $${(599 + 295) / 100}`);
 // alert(`3i:\nTotal cost: $${(599+295)/100}`);
 // alert(`3j:\nTotal cost: $${(599+295)/100}\nThank you,come again`);
-console.log(`3k\nitems(${2+2}): $${((2*2095)+(2*799))/100}`)
-console.log(`3l\nShipping & handling: $${(499+499)/100}`)
-console.log(`3m\nTotal before tax: $${((2*2095)+(2*799) + (499+499))/100 }`)
-console.log(`3n\nEstimated tax(10%): $${Math.round((2*2095+2*799 +499+499)*0.1)/100 }`)
-console.log(`3n\nOrder total: $${Math.round(((2*2095+2*799 +499+499)*0.1)+(2*2095+2*799 + 499+499))/100 }`)
+console.log(`3k\nitems(${2 + 2}): $${((2 * 2095) + (2 * 799)) / 100}`)
+console.log(`3l\nShipping & handling: $${(499 + 499) / 100}`)
+console.log(`3m\nTotal before tax: $${((2 * 2095) + (2 * 799) + (499 + 499)) / 100}`)
+console.log(`3n\nEstimated tax(10%): $${Math.round((2 * 2095 + 2 * 799 + 499 + 499) * 0.1) / 100}`)
+console.log(`3n\nOrder total: $${Math.round(((2 * 2095 + 2 * 799 + 499 + 499) * 0.1) + (2 * 2095 + 2 * 799 + 499 + 499)) / 100}`)
+
+// alert(`items(${2 + 2}): $${((2 * 2095) + (2 * 799)) / 100}\nShipping & handling: $${(499 + 499) / 100}\nTotal before tax: $${((2 * 2095) + (2 * 799) + (499 + 499)) / 100} \nEstimated tax(10%): $${Math.round((2 * 2095 + 2 * 799 + 499 + 499) * 0.1) / 100}\nOrder total: $${Math.round(((2 * 2095 + 2 * 799 + 499 + 499) * 0.1) + (2 * 2095 + 2 * 799 + 499 + 499)) / 100}`)
 
