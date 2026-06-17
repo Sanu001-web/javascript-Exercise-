519d34bb24058176f5136c9da8b34cf743c6db7f
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-06-17 15:39:12 +0530

    Implement for and while loops in lesson-11-prac.js
    Add loops to lesson-11-prac.js for practice exercises.

---
+++ JavaScript Prectice/lesson-11-prac.js
@@ -0,0 +1,21 @@
+// 11d,e,f
+
+for(let i=0; i<10;i+=2){
+console.log(i);
+}
+
+for(let i=5; i>=0;i--){
+console.log(i);
+}
+
+}
+let i=0;
+while(i<=10){
+console.log(i);
+    i+=2;
+}
+
+let i = 5;
+while(i>=0){
+    console.log(i);
+    i--;
