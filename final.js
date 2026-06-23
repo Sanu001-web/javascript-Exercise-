8b2c060946daa95a3e41bceb5cd83bcf2dae9ca2
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-06-23 14:59:18 +0530

    Implement todo list functionality in final.js

---
+++ final.js
@@ -0,0 +1,55 @@
+const todoList = [{
+  name: 'Drink food',
+  dueDate: '27-06-26'
+},{
+  name: 'Play water',
+  dueDate: '25-06-26'
+}];
+randderTodoList();
+function randderTodoList() {
+
+  let todoHtml = '';
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    // const name = todoObject.name;
+    const { name, dueDate } = todoObject;
+    // const {dueDate} = todoObject;
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick=
+    "todoList.splice(${i},1)
+    randderTodoList();
+    " class="js-delete-btn">
+    Delete
+    </button>
+    `
+    todoHtml += html;
+  }
+  // console.log(todoHtml);
+
+  document.querySelector('.displayTodos').innerHTML = todoHtml;
+}
+
+function addTodo() {
+  const todoElement = document.querySelector('.js-input');
+  const dueDateElement = document.querySelector('.js-dates');
+  const name = todoElement.value;
+  const dueDate = dueDateElement.value;
+  // console.log(name);
+  todoList.push({
+    // name: name,
+    // dueDate: dueDate
+    name,
+    dueDate
+  });
+  // console.log(todoList);
+  todoElement.value = '';
+  for (let index = 0; index <= todoList.length - 1; index++) {
+    const itemsTodo = todoList[index];
+    // console.log(itemsTodo);
+  }
+  document.querySelector('.displayTodos').innerHTML =
+    '';
+  randderTodoList();
+}
