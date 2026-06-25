fc22d908bb2a6e2646856e3d565530e7575b84f9
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-06-25 15:59:12 +0530

    Add functions to find index and get unique values

--- JavaScript Prectice/lesson-11.js
+++ JavaScript Prectice/lesson-11.js
@@ -381,3 +381,28 @@ console.log('11-V solution');
   }
   console.log(i);
  }
+
+// 11-W solution vvvvvvvvvvvvvvvvvvv-vvvvvvvvvvvvvvvvvv-------------vvvvvvvvvvvv
+
+console.log('11-W solution');
+function findIndex(array, word) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === word) {
+      return i;
+    }
+  }
+  return -1;
+}
+function unique(array) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+      const word = array[i];
+    if (findIndex(result, word) === -1) {
+      result.push(word);
+    }
+  }
+  return result;
+}
+
+console.log(unique(['green', 'red', 'blue', 'red'], 'red'));
+console.log(unique(['red', 'green', 'green', 'red']));
