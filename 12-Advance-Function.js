56cf71c421ca567cc792671f65622ef09189c9db
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-06-29 15:58:05 +0530

    Refactor array methods and update console logs
    In this lesson:
    1. Functions are values
    2. setTimeout(), setInterval()
    3. .forEach()
    4. Arrow functions, .addEventListener()
    5. .filter(), .map()

--- 12-Advance-Function.js
+++ 12-Advance-Function.js
@@ -31,14 +31,101 @@ setIntervall(function greenTwo(){
 },2000);
 
 console.log('this is next called next line')
-*/
+
+
 [ 'make water',
   'play movie',
-  'drink food'
-].forEach(function (value, index) {
+  'drink food',
+  'play road'
+].forEach((value, index) => {
+
   if(value ==='play movie'){
-    return;
+    return; //continu;
   }
-  console.log(index)
-  console.log(value);
+
+  console.log(`${index} : ${value}`)
+  // console.log(value);
 });
+
+
+const fruit=['apple','banana','orang','grape'];
+
+fruit.forEach((index,value)=>{
+  console.log(`${index} : ${value}`);
+});
+
+const aeroFunction= (name)=>{
+  console.log('Helllo from',name);
+}
+
+aeroFunction('shaan !');
+
+
+const aerroFunction= function(){
+  console.log('type function!');
+}
+aerroFunction();
+
+function helloFunction(){
+  console.log('?')
+}
+helloFunction();
+
+const nums = [1, 2, 3];
+
+nums.forEach((value, index, array) => {
+    console.log(value, index, array);
+});
+
+const object ={
+  methodZero: ()=>{
+    console.log('aero method!');
+  },
+  method(){
+    console.log('shorthand method!');
+  }
+};
+
+object.methodZero();
+object.method();
+
+const buttonElement = document.querySelector('.js-click-btn');
+
+const eventListener = () => {
+  console.log('clicked!');
+  // document.querySelector('.radderpro').innerHTML='clicked';
+  //if we want to saw result on screen:
+}
+
+buttonElement.addEventListener('click',eventListener);
+
+// buttonElement.removeEventListener('click',eventListener);
+
+buttonElement.addEventListener('click', () => {
+  console.log('clicked-Two!');
+  // document.querySelector('.radderpro').innerHTML='clicked';
+  //if we want to saw result on screen:
+})
+*/
+
+
+
+console.log('filter():', [4, 5, -8, -5, 58].filter((value, index) => {
+
+  /*if(value >=0){
+    return true
+  }
+  else{
+  return false;
+  }
+  */
+  return value >= 0;
+
+}));
+
+console.log('map():', [5, 2, 3].map((value, index) => {
+  return value * 2;
+}));
+
+// shortcuts:
+console.log('map():', [15, 21, 35].map(value => value * 2));
