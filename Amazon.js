1ec453f4817c67572021dc2284d4f5382c12fcd4
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-07-03 15:43:56 +0530

    Update product display and add to cart logic
    Refactor product rendering and cart functionality.

--- Amazon.js
+++ Amazon.js
@@ -1,54 +1,8 @@
-const product = [
-  {
-    image: 'https://tinyurl.com/u3h63zff',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-      starsImg:'https://tinyurl.com/3ypcm837',
-      start: 4.5,
-      count: 87
-    },
-    priceCent: 1092
-  }, {
-    image: 'https://tinyurl.com/479fmjbs',
-    name: 'Intermediate Size Basketball',
-    rating: {
-      starsImg:'https://tinyurl.com/8krsstx3',
-      stars: 3,
-      count: 127
-    },
-    priceCent: 2095
-  }, {
-    image: 'https://tinyurl.com/ys9fkmj3',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-      starsImg:'https://tinyurl.com/3ypcm837',
-      stars: 4.5,
-      count: 56
-    },
-    priceCent: 799
-  }, {
-    image: 'https://tinyurl.com/3dnydjbv',
-    name: 'Black Athletic Cotton Beg -Men',
-    rating: {
-      starsImg:'https://tinyurl.com/3ypcm837',
-      stars: 4.5,
-      count: 96
-    },
-    priceCent: 1590
-  }, {
-    image: 'https://tinyurl.com/4rnrzu69',
-    name: 'Black and Gray Athletic Cotton Shoes',
-    rating: {
-      starsImg:'https://tinyurl.com/3ypcm837',
-      stars: 4.5,
-      count: 155
-    },
-    priceCent: 5090
-  }
-]
 
-product.forEach((product) => {
-  const html =
+let productsHTML = '';
+
+products.forEach((product) => {
+  productsHTML +=
     `
 <div class="product-container">
       <div class="product-image-container">
@@ -69,7 +23,7 @@ product.forEach((product) => {
       </div>
 
       <div class="product-price">
-        ${product.priceCent/100}
+        $${(product.priceCents / 100).toFixed(2)}
       </div>
 
       <div class="product-quantity-container">
@@ -94,10 +48,63 @@ product.forEach((product) => {
             Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id="${product.id}">
             Add to Cart
           </button>
     </div>
 `
-console.log(html);
-})
+});
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart-button').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      });
+    }
+
+
+  let cartQuantit =0;
+
+  cart.forEach((item)=>{
+  cartQuantit +=item.quantity;
+  });
+
+document.querySelector(".js-cart-quantity").innerHTML=cartQuantit;
+
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
