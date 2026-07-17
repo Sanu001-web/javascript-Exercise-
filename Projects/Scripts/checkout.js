b0d25e5b0697e729d3d34573ae92edbc684081ee
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-07-17 15:22:17 +0530

    Add calculateCartQuantity function to checkout.js

--- Projects/Scripts/checkout.js
+++ Projects/Scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from '/data/cart.js';
+import { cart, removeFromCart,calculateCartQuantity } from '/data/cart.js';
 import { products } from '/data/products.js';
 import { formatCurrency } from '/utils/money.js';
 let cartSummaryHTML = '';
@@ -93,6 +93,7 @@ cart.forEach((cartItem) => {
           </div>
 `;
 
+
 });
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 document.querySelectorAll('.js-delete-quantity-link').forEach((link) => {
@@ -109,11 +110,7 @@ document.querySelectorAll('.js-delete-quantity-link').forEach((link) => {
 });
 
 function updateCartQuantity(){
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
+  const cartQuantity =calculateCartQuantity();
   document.querySelector('.js-return-to-home-link')
     .innerHTML = `${cartQuantity} items`;
 }
