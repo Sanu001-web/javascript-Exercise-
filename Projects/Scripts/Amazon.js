35ad1dc9a80973a4d35dda27faefcc71d6a10308
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-07-22 13:02:56 +0530

    Fix formatting and spacing in Amazon.js

--- Projects/Scripts/Amazon.js
+++ Projects/Scripts/Amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart,calculateCartQuantity} from '/data/cart.js';
+import { cart, addToCart, calculateCartQuantity } from '/data/cart.js';
 import { products } from '/data/products.js';
 import { formatCurrency } from '/utils/money.js';
 
@@ -67,7 +67,7 @@ document.querySelector('.js-products-grid')
 
 function updateCartQuantity() {
 
- const cartQuantity = calculateCartQuantity();
+  const cartQuantity = calculateCartQuantity();
 
   document.querySelector(".js-cart-quantity")
     .innerHTML = cartQuantity;
