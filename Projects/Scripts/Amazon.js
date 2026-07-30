54e394fe455afe6e4a685d1b139e8293101287fe
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-07-30 15:26:43 +0530

    Update product rating and price display methods

--- Projects/Scripts/Amazon.js
+++ Projects/Scripts/Amazon.js
@@ -2,6 +2,7 @@ import { cart, addToCart, calculateCartQuantity } from '/data/cart.js';
 import { products } from '/data/products.js';
 import { formatCurrency } from '/utils/money.js';
 
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -19,14 +20,14 @@ products.forEach((product) => {
 
       <div class="product-rating-container">
         <img class="product-rating-stars"
-              src="${product.rating.starsImg}">
+              src="${product.getStarsUrl()}">
           <div class="product-rating-count link-primary">
             ${product.rating.count}
           </div>
       </div>
 
           <div class="product-price">
-            $${formatCurrency(product.priceCents)}
+            ${product.getPrice()}
           </div>
 
           <div class="product-quantity-container">
