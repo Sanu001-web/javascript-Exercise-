97b9ed72d5330b222ae64736688e1a2b5c63f2f0
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-08-01 15:10:01 +0530

    Comment out date logging in products.js
    Commented out date logging code for future reference.

--- Projects/Scripts/data/products.js
+++ Projects/Scripts/data/products.js
@@ -53,6 +53,15 @@ class Clothing extends Product {
 
 }
 
+/*
+const date = new Date();
+
+console.log(date);
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleTimeString());
+console.log(date.toUTCString());
+*/
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
