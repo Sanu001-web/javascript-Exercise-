8d0fc63722ab82d345d94cda9537a27ae0c36af7
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-08-07 14:06:37 +0530

    Add error handling example in errorCatchAsync.js

---
+++ errorCatchAsync.js
@@ -0,0 +1,12 @@
+// function functionddd(){
+//   console.log('hello dosto')
+// }
+// if is not exist
+
+try {
+  functionddd();
+  console.log('next line');
+}
+catch(error){
+  console.log('error is coming');
+}
