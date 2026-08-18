94321d56e83ef2b7381de17fbdfacec18e0943fb
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-08-18 13:43:01 +0530

    Refactor videoInfo and renderVideos for export

--- Projects/Youtube/Scripts/videosInfo.js
+++ Projects/Youtube/Scripts/videosInfo.js
@@ -1,4 +1,4 @@
-const videoInfo = [
+export const videoInfo = [
   {
     thubmnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHvKnwO4o06B7MgtgP-k4CUz24xy-5siK7dw&s',
     profilePic: 'https://tinyurl.com/2mrsdcv6',
@@ -30,13 +30,43 @@ const videoInfo = [
     }
   },
   {
-    thubmnail: 'https://tinyurl.com/2yzt95s7',
+    thubmnail: 'https://tinyurl.com/3ymerhes',
     profilePic: 'https://tinyurl.com/2set23hh',
-    videoTitle: 'The Simplest Math Problem No- Collatz Conjecture',
+    videoTitle: '5 Powerful Lessons that helped me in Life #motivation',
     videoAuthor: 'Veritasium',
     channelViewInfo: {
       views: '18M views',
       UploadDate: '4 months ago'
+    }
+  },
+  {
+    thubmnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHvKnwO4o06B7MgtgP-k4CUz24xy-5siK7dw&s',
+    profilePic: 'https://tinyurl.com/2mrsdcv6',
+    videoTitle: 'Talking Tech and AI with Google CEO Sundar Pichai!',
+    videoAuthor: 'Marques Brownlee',
+    channelViewInfo: {
+      views: '3.4M views',
+      UploadDate: '6 months ago'
+    }
+  },
+  {
+    thubmnail: 'https://tinyurl.com/3239aswr',
+    profilePic: 'https://tinyurl.com/3j4y5dkc',
+    videoTitle: 'Try Not To Laugh Challenge #9',
+    videoAuthor: 'Markiplier',
+    channelViewInfo: {
+      views: '19M views',
+      UploadDate: '4 years ago'
+    }
+  },
+  {
+    thubmnail: 'https://tinyurl.com/tbjvav3w',
+    profilePic: 'https://tinyurl.com/wm55uces',
+    videoTitle: 'Crazy Tik Toks Taken Moments Before DISASTER',
+    videoAuthor: 'SSSniperWolf',
+    channelViewInfo: {
+      views: '12M views',
+      UploadDate: '1 year ago'
     }
   },
   {
@@ -50,9 +80,9 @@ const videoInfo = [
     }
   },
   {
-    thubmnail: 'https://tinyurl.com/ykpnv6ar',
+    thubmnail: 'https://tinyurl.com/2p9zabxs',
     profilePic: 'https://tinyurl.com/24yyfj4f',
-    videoTitle: 'Anything You Can Fit In The Circle I’ll Pay For',
+    videoTitle: 'Building software is not problem solving',
     videoAuthor: 'MrBeast',
     channelViewInfo: {
       views: '141M views',
@@ -63,20 +93,20 @@ const videoInfo = [
 ];
 
 
-function renderVideos() {
+export function renderVideos(videos) {
   const container = document.querySelector('.js-video-grid');
 
   let videoGridHtml = '';
 
-  videoInfo.forEach(video => {
+  videos.forEach(video => {
     videoGridHtml += `
     
-<div class="video-preview">
-<a href="youtube.com/watch?v=Tck7MFXRQZg&utm_source=chatgpt.com" target="_blank">
-      <div class="thumbnail-row">
-        <div class="video-time">14:20</div>
-        <img class="thumbnail" src="${video.thubmnail}">
-      </div>
+    <div class="video-preview">
+      <a href="https://youtu.be/0wffuBWP5xg?si=NIte7jO18XNdGpmv">
+        <div class="thumbnail-row">
+          <div class="video-time">14:20</div>
+          <img class="thumbnail" src="${video.thubmnail}">
+        </div>
       </a>
       <div class="video-info-grid">
         <div class="channel-picture">
@@ -104,7 +134,6 @@ function renderVideos() {
   container.innerHTML = videoGridHtml;
 }
 renderVideos(videoInfo);
-
 
 
 
