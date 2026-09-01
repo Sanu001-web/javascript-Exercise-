f8c2bf55e5a1c3b92f895496f2b8b401140bebb2
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-09-01 15:58:42 +0530

    Add music content data structure
    This file contains music content data including biggest hits, weekly top songs, and discover playlists with associated images and alt text.

---
+++ Projects/Youtube/data/musicContent.js
@@ -0,0 +1,90 @@
+export const musicContent = {
+  biggestHits: [
+    {
+      imageBack: 'https://tinyurl.com/43x4htrs',
+      imageMiddle: 'https://tinyurl.com/3cu78r4a',
+      imageFront: 'https://tinyurl.com/4b9rht7e',
+      alt: "India's biggest hits playlist",
+      overlay: '▶ Play all'
+    },
+    {
+      imageBack: 'https://tinyurl.com/43x4htrs',
+      imageMiddle: 'https://tinyurl.com/3cu78r4a',
+      imageFront: 'https://tinyurl.com/4r5y7ent',
+      alt: 'Top Indian music playlist',
+      overlay: '▶ Play all'
+    },
+    {
+      imageBack: 'https://tinyurl.com/43x4htrs',
+      imageMiddle: 'https://tinyurl.com/3cu78r4a',
+      imageFront: 'https://tinyurl.com/msuwu5yt',
+      alt: 'Popular Hindi songs playlist',
+      overlay: '▶ Play all'
+    },
+    {
+      imageBack: 'https://tinyurl.com/43x4htrs',
+      imageMiddle: 'https://tinyurl.com/3cu78r4a',
+      imageFront: 'https://tinyurl.com/5xbwencw',
+      alt: 'Popular Hindi songs playlist',
+      overlay: '▶ Play all'
+    },
+    {
+      imageBack: 'https://tinyurl.com/43x4htrs',
+      imageMiddle: 'https://tinyurl.com/3cu78r4a',
+      imageFront: 'https://tinyurl.com/3jzh5dn6',
+      alt: 'Popular Hindi songs playlist',
+      overlay: '▶ Play all'
+    }
+  ],
+
+  weeklyTop: [
+    {
+      imageFront: 'https://tinyurl.com/5n7m6vs9',
+      alt: "India's biggest hits playlist"
+    },
+    {
+      imageFront: 'https://tinyurl.com/5h96r8p7',
+      alt: 'Top Indian music playlist'
+    },
+    {
+      imageFront: 'https://tinyurl.com/23xd9kc4',
+      alt: 'Popular Hindi songs playlist'
+    },
+    {
+      imageFront: 'https://tinyurl.com/2eu9nv5m',
+      alt: 'Latest music playlist'
+    },
+    {
+      imageFront: 'https://tinyurl.com/va4jvf9x',
+      alt: 'Bollywood hits playlist'
+    }
+  ],
+
+  discover: [
+    {
+      imageFront: 'https://tinyurl.com/n22v3buz',
+      alt: "India's biggest hits playlist"
+    },
+    {
+      imageFront: 'https://tinyurl.com/42zw53vm',
+      alt: 'Top Indian music playlist'
+    },
+    {
+      imageFront: 'https://tinyurl.com/m54vtn8m',
+      alt: 'Popular Hindi songs playlist'
+    },
+    {
+      imageFront: 'https://tinyurl.com/48xm7xkk',
+      alt: 'Latest music playlist'
+    },
+    {
+      imageFront: 'https://tinyurl.com/yxwbn8d6',
+      alt: 'Bollywood hits playlist'
+    }
+  ]
+};
+
+export const defaultImages = {
+  imageBack: 'https://tinyurl.com/43x4htrs',
+  imageMiddle: 'https://tinyurl.com/3cu78r4a'
+};
