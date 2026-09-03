9c17b8255208eb93e0fe221da9ac95527c0e07ec
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-09-03 15:52:03 +0530

    Enhance subscription and join button functionality
    Refactor subscription functionality and add local storage for subscriptions and notifications.

--- Projects/Youtube/Scripts/subscription.js
+++ Projects/Youtube/Scripts/subscription.js
@@ -1,5 +1,6 @@
 export function subsFunc() {
   const subscribeButton = document.querySelector('.subscribe-button');
+  const notificationBell = document.querySelector('.js-notification-bell');
 
   subscribeButton?.addEventListener('click', () => {
     const isSubscribed = subscribeButton.innerText === 'Subscribed';
@@ -7,20 +8,37 @@ export function subsFunc() {
     subscribeButton.innerText = isSubscribed ? 'Subscribe' : 'Subscribed';
 
     subscribeButton.classList.toggle('is-sub-btn', !isSubscribed);
+    // Show bell after subscribing
+    notificationBell?.classList.toggle('show-bell', !isSubscribed);
 
   });
 }
 
-export function joinFunc(){
-const joinButton = document.querySelector('.js-join-button');
 
-joinButton?.addEventListener('click', () => {
-  if (joinButton.innerText === 'Join') {
-    joinButton.innerText = 'Joined';
-    joinButton.classList.add('joined');
-  } else {
-    joinButton.innerText = 'Join';
-    joinButton.classList.remove('joined');
-  }
-});
+
+localStorage.setItem(
+  "subscriptions",
+  JSON.stringify(["channel001", "channel005"])
+);
+
+localStorage.setItem(
+  "notifications",
+  JSON.stringify({
+    channel001: true
+  })
+);
+
+
+export function joinFunc() {
+  const joinButton = document.querySelector('.js-join-button');
+
+  joinButton?.addEventListener('click', () => {
+    if (joinButton.innerText === 'Join') {
+      joinButton.innerText = 'Joined';
+      joinButton.classList.add('joined');
+    } else {
+      joinButton.innerText = 'Join';
+      joinButton.classList.remove('joined');
+    }
+  });
 }
