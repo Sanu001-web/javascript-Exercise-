4e3a13e2071b5194e1f8fded7e4aec1f4fdefb6a
Sanu Kayda <91690368+Sanu001-web@users.noreply.github.com>
2026-09-14 12:29:09 +0530

    Refactor SVG attributes for consistency

--- Projects/Youtube/Scripts/sideBar.js
+++ Projects/Youtube/Scripts/sideBar.js
@@ -10,8 +10,7 @@ export function renderSidebar() {
         <!-- DARK MODE ICON -->
         <svg class="home-icon icon-light" 
           xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-          fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-          class="lucide lucide-house-icon lucide-house">
+          fill="none"           stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
           <path
             d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
@@ -66,8 +65,7 @@ export function renderSidebar() {
             stroke="currentColor"
             stroke-width="2" 
             stroke-linecap="round" 
-            stroke-linejoin="round"
-            class="lucide lucide-compass">
+            stroke-linejoin="round">
             <circle cx="12" cy="12" r="10" />
             <path d="m16.24 7.76-1.804 5.411a2 2 0 0 1-1.265 1.265L7.76 16.24l1.804-5.411a2 2 0 0 1 1.265-1.265z" />
          </svg>
@@ -151,4 +149,3 @@ export function renderSidebar() {
 
 }
 
-renderSidebar();
